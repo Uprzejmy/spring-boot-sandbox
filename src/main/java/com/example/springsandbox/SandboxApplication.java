8c90bd197b4d5782092fb7020ced6a41020cc362
Uprzejmy <lmerynda@gmail.com>
2023-07-03 20:58:49 +0000

    logging example

--- src/main/java/com/example/springsandbox/SandboxApplication.java
+++ src/main/java/com/example/springsandbox/SandboxApplication.java
@@ -1,5 +1,7 @@
 package com.example.springsandbox;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class SandboxApplication {
 
+    Logger logger = LoggerFactory.getLogger(SandboxApplication.class);
+
     @RequestMapping("/")
     String home() {
+        logger.info("/ resource requested");
         return "Hello World!";
     }
 
